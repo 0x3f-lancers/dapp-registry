820f50e39d23064510b68e9832335126d1a9608e
trkraza <trkraza0204@gmail.com>
2026-02-11 18:39:04 +0530

    test: validate wrong path issue

--- scripts/run-validate.ts
+++ scripts/run-validate.ts
@@ -1,7 +1,7 @@
 import path from "path";
 import validate from "./validate";
 
-const appsDir = path.join(process.cwd(), "data", "apps");
+const appsDir = path.join(process.cwd(), "source", "apps");
 validate(appsDir).catch(() => {
   process.exit(1);
 });
